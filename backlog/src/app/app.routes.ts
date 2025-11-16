8ea10d5f102c978e7f2e40cf3218917d7af9aeb1
Sebastian Renner <sebastian.renner@students.fhnw.ch>
2025-11-16 17:56:58 +0100

    Add first feature component and some initial cleanup

--- backlog/src/app/app.routes.ts
+++ backlog/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProjectOverview } from './features/project-overview/project-overview';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ProjectOverview
+    }
+];
