f35d9aad1924f8cae3bc048ff0748cf396507821
Sebastian Renner <sebastian.renner@students.fhnw.ch>
2025-11-16 20:51:00 +0100

    introduced dynamic color for dot based on text

--- backlog/src/app/components/project-card/language-display/language-display.ts
+++ backlog/src/app/components/project-card/language-display/language-display.ts
@@ -8,4 +8,17 @@ import { Component, input } from '@angular/core';
 })
 export class LanguageDisplay {
   language = input.required<string>();
+
+  textToColor(text: string): string {
+    let r = 0, g = 0, b = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      const charCode = text.charCodeAt(i);
+      r = (r + charCode * 1) % 256;
+      g = (g + charCode * 2) % 256;
+      b = (b + charCode * 3) % 256;
+    }
+
+    return `rgb(${r}, ${g}, ${b})`;
+  }
 }
