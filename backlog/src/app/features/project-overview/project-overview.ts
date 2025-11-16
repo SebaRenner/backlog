f17a933fa1a489e3347652c9be734a3765de4aa9
Sebastian Renner <sebastian.renner@students.fhnw.ch>
2025-11-16 18:49:27 +0100

    Added project card component

--- backlog/src/app/features/project-overview/project-overview.ts
+++ backlog/src/app/features/project-overview/project-overview.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ProjectCard } from '../../components/project-card/project-card';
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-project-overview',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, ProjectCard],
   templateUrl: './project-overview.html',
   styleUrl: './project-overview.css',
 })
 export class ProjectOverview {
-
+    projects: Project[] = [{
+      name: 'ngx-interceptors',
+      language: 'TypeScript',
+      description: 'Library with common HTTP Interceptors for Angular Applications'
+    }, 
+    {
+      name: 'quantum-random',
+      language: 'Q#',
+      description: 'Simple stupid random number generator using Q#.'
+    }];
 }
