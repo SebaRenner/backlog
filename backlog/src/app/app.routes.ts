a354b475a8e99beed7733ba7d9aa0fce9eab3949
Sebastian Renner <sebastian.renner@students.fhnw.ch>
2025-11-21 23:36:51 +0100

    Introduced a project store using ngrx signals store

--- backlog/src/app/app.routes.ts
+++ backlog/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ProjectOverview } from './features/project-overview/project-overview';
+import { Board } from './features/board/board';
 
 export const routes: Routes = [
     {
         path: '',
         component: ProjectOverview
+    },
+    {
+        path: 'board/:projectId',
+        component: Board
     }
 ];
