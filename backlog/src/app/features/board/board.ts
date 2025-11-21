8baa4340764893bcd841e925fcb9cdefbcfffec8
Sebastian Renner <sebastian.renner@gmx.ch>
2025-11-21 23:49:15 +0100

    Load project from store

--- backlog/src/app/features/board/board.ts
+++ backlog/src/app/features/board/board.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GitHubRepository } from '../../models/github.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ProjectStore } from '../../store/project.store';
 
 @Component({
   selector: 'app-board',
@@ -7,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './board.css',
 })
 export class Board {
-  
+  project: GitHubRepository | undefined;
+
+  private readonly projectStore = inject(ProjectStore);
+
+  constructor(route: ActivatedRoute) {
+    route.params.pipe(takeUntilDestroyed()).subscribe((params) => {
+      const projectId = params['projectId'];
+      this.project = this.projectStore.projects().find(p => p.id == projectId);
+    })
+  }
 }
