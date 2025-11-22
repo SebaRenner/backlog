31ed78878c08b9f31d6bf045506de8c5373f33ee
Sebastian Renner <sebastian.renner@gmx.ch>
2025-11-22 15:31:56 +0100

    Added swimlane component

--- backlog/src/app/features/board/board.ts
+++ backlog/src/app/features/board/board.ts
@@ -3,15 +3,18 @@ import { ActivatedRoute } from '@angular/router';
 import { GitHubRepository } from '../../models/github.model';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ProjectStore } from '../../store/project.store';
+import { Swimlane } from '../../components/swimlane/swimlane';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [Swimlane],
   templateUrl: './board.html',
   styleUrl: './board.css',
 })
 export class Board {
   project: GitHubRepository | undefined;
+
+  readonly swimlanes = ['New', 'Next Up', 'In Progress', 'Review', 'Done'];
 
   private readonly projectStore = inject(ProjectStore);
 
