037dcfc06d89bfcb6356beed1d0d2d931ece1486
Sebastian Renner <sebastian.renner@gmx.ch>
2025-11-24 14:01:27 +0100

    wip drag and drop

--- backlog/src/app/components/swimlane/swimlane.ts
+++ backlog/src/app/components/swimlane/swimlane.ts
@@ -1,13 +1,18 @@
 import { Component, input } from '@angular/core';
 import { SwimlaneModel } from '../../models/board.model';
 import { WorkItemDisplay } from '../work-item-display/work-item-display';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-swimlane',
-  imports: [WorkItemDisplay],
+  imports: [WorkItemDisplay, DragDropModule],
   templateUrl: './swimlane.html',
   styleUrl: './swimlane.css',
 })
 export class Swimlane {
     model = input.required<SwimlaneModel>();
+
+    onDrop(event: unknown) {
+      console.log(event);
+    }
 }
