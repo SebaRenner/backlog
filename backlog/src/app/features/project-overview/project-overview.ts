47e0438a91320788d02cd879f9d24662ccd5722b
Sebastian Renner <sebastian.renner@gmx.ch>
2025-11-25 08:36:59 +0100

    Removed unused button on overview page and added header instead

--- backlog/src/app/features/project-overview/project-overview.ts
+++ backlog/src/app/features/project-overview/project-overview.ts
@@ -1,6 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { ProjectCard } from '../../components/project-card/project-card';
 import { GitHubRepository } from '../../models/github.model';
 import { Router } from '@angular/router';
@@ -8,7 +6,7 @@ import { ProjectStore } from '../../store/project.store';
 
 @Component({
   selector: 'app-project-overview',
-  imports: [MatButtonModule, MatIconModule, ProjectCard],
+  imports: [ProjectCard],
   templateUrl: './project-overview.html',
   styleUrl: './project-overview.css',
 })
