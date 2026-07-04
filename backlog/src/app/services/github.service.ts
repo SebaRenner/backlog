b5d3e9f4103b1c59fb9653c2d25acd5ecc11e5f1
SebaRenner <sebastian.renner@gmx.ch>
2026-07-04 19:24:38 +0200

    fix: fixed getAllRepos only loading 30 items

--- backlog/src/app/services/github.service.ts
+++ backlog/src/app/services/github.service.ts
@@ -1,23 +1,36 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, tap } from "rxjs";
+import { EMPTY, Observable } from "rxjs";
+import { expand, reduce } from "rxjs/operators";
 import { GitHubRepository } from "../models/github.model";
 import { environment } from "../environments/environment";
 
 @Injectable({ providedIn: 'root' })
 export class GitHubService {
-    readonly baseUrl = 'https://api.github.com/';
-    readonly token = environment.githubToken;
+    private readonly baseUrl = 'https://api.github.com/';
+    private readonly token = environment.githubToken;
+    private readonly pageSize = 100;
 
     constructor(private http: HttpClient) {}
 
     getAllRepos(): Observable<GitHubRepository[]> {
-        const endpoint = `${this.baseUrl}user/repos`;
+        return this.fetchReposPage(1).pipe(
+            expand((repos, index) =>
+                repos.length === this.pageSize
+                    ? this.fetchReposPage(index + 2)
+                    : EMPTY
+            ),
+            reduce((all, repos) => all.concat(repos), [] as GitHubRepository[])
+        );
+    }
+
+    private fetchReposPage(page: number): Observable<GitHubRepository[]> {
+        const endpoint = `${this.baseUrl}user/repos?per_page=${this.pageSize}&page=${page}`;
         const headers = new HttpHeaders({
             'Authorization': `Bearer ${this.token}`,
             'Accept': 'application/vnd.github+json'
         });
-        
+
         return this.http.get<GitHubRepository[]>(endpoint, { headers });
     }
 }
