7f64daa62996e9f65513f51d1ecf177032144ac5
SebaRenner <sebastian.renner@gmx.ch>
2026-07-05 09:33:20 +0200

    style: lint fixes

--- src/app/guards/login-guard.ts
+++ src/app/guards/login-guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn, Router } from '@angular/router';
 import { LoginStore } from '../store/login.store';
 import { inject } from '@angular/core';
 
-export const loginGuard: CanActivateFn = (route, state) => {
+export const loginGuard: CanActivateFn = (_, __) => {
   const loginStore = inject(LoginStore);
   const router = inject(Router);
 
